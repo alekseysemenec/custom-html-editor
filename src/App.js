f41849c72b9f0e5ab11c9136140c66539c9f4fdf
semenecAleksey <aleksey.semenec.dev@gmail.com>
2019-12-17 23:15:44 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import ControlPanel from "./control-panel/ControlPanel";
 import FileZone from "./file-zone/FileZone";
-import getMockText from './text.service';
+import getMockText from "./text.service";
+import wrapRange from 'wrap-range-text';
+
+const onModifySelectedWord = ({ tag, selected, value }) => {
+    let updatedHTML = value;
+    const selectedWihTag = `<${tag}>${selected}</${tag}>`;
+
+    if (value.indexOf(selectedWihTag) !== -1) {
+        updatedHTML = value.replace(selectedWihTag, selected);
+    } else {
+        updatedHTML = value.replace(selected, selectedWihTag);
+    }
+
+    return updatedHTML;
+}
 
 class App extends Component {
-    getText() {
-        getMockText().then(function (result) {
-            console.log(result);
+    state = {
+        value: "",
+        selected: null
+    };
+
+    getText = async () => {
+        const result = await getMockText();
+
+        this.setState({
+            value: result
         });
+    };
+
+    handleChangeEditor = value => {
+        this.setState({
+            value
+        });
+    };
+
+    handleSelect = selected => {
+        this.setState({
+            selected
+        });
+    };
+
+    handleModifySelected = tag => {
+        const { selected, value } = this.state;
+
+        if (selected) {
+            const updatedHTML = onModifySelectedWord({
+                tag,
+                selected,
+                value
+            })
+
+            this.setState({
+                value: updatedHTML,
+            })
+        }
+    };
+
+    componentDidMount() {
+        this.getText();
     }
+
     render() {
+        const { value, selected } = this.state;
+
         return (
             <div className="App">
                 <header>
                     <span>Simple Text Editor</span>
                 </header>
                 <main>
-                    <ControlPanel/>
-                    <FileZone/>
+                    <ControlPanel onModify={this.handleModifySelected} />
+                    <FileZone
+                        value={value}
+                        onChange={this.handleChangeEditor}
+                        handleSelect={this.handleSelect}
+                    />
                 </main>
             </div>
         );
--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
-import './FileZone.css';
+import React, { Component } from "react";
+import "./FileZone.css";
 
 class FileZone extends Component {
+    fileZoneAnchor = null;
+
+    handleInputFileZone = () => {
+        const { onChange } = this.props;
+
+        onChange(this.fileZoneAnchor.innerHTML);
+    };
+
+    handleSelect = () => {
+        const { handleSelect } = this.props;
+
+        const selected = window.getSelection().toString();
+        if (!!selected) {
+            handleSelect(selected);
+        }
+    };
+
     render() {
+        const { value } = this.props;
+
         return (
             <div id="file-zone">
-                <div id="file">
-
-                </div>
+                <div
+                    ref={node => (this.fileZoneAnchor = node)}
+                    contentEditable
+                    id="file"
+                    dangerouslySetInnerHTML={{ __html: value }}
+                    onInput={this.handleInputFileZone}
+                    onClick={this.handleSelect}
+                ></div>
             </div>
         );
     }
